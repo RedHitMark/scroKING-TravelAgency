449df6f081622dec03a2ed87b57b7644c80afa73
Giovanni Ciaramella <giova4698@gmail.com>
2019-08-07 02:02:48 +0200

    script jquery per lo scroll

---
+++ JS/scroll.js
@@ -0,0 +1,15 @@
+
+    $('#scroll').hide();
+    let visible = false;
+    $('#moreButton').click(function(e){
+        e.preventDefault();
+        $('#scroll').toggle("slow");
+        if(visible == false){
+            $("#moreButton").prop('value', 'Mostra meno risultati');
+            
+        visible = true;
+        }else if( visible == true){
+            $("#moreButton").prop('value', 'Mostra altri risultati');
+            visible = false;
+        }
+    });
