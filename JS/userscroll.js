e8263efe66a14731fcae3507ca44b6ad7727cec0
vigimella <giova4698@gmail.com>
2019-08-11 21:16:31 +0200

    costruzione pagina livello scrocco e completamento user page

--- JS/userscroll.js
+++ JS/userscroll.js
@@ -5,23 +5,23 @@ $('.previus-trips h3').click(function (e) {
     e.preventDefault();
     $('.previus-trips p').toggle("slow");
     if (visible == false) {
-        var collapsed=$(this).find('i').hasClass('fas fa-arrow-down');
+        var collapsed=$(this).find('i').hasClass('fas fa-angle-down');
     
-    $('.faq-links').find('i').removeClass('fas fa-arrow-down');
+    $('.faq-links').find('i').removeClass('fas fa-angle-down');
     
     $('.faq-links').find('i').addClass('fas fa-arrow-up');
     if(collapsed)
-    $(this).find('i').toggleClass('fas fa-arrow-down fas fa-arrow-up');
+    $(this).find('i').toggleClass('fas fa-angle-down fas fa-angle-up');
 
         visible = true;
     } else if (visible == true) {
-        var collapsed=$(this).find('i').hasClass('fas fa-arrow-up');
+        var collapsed=$(this).find('i').hasClass('fas fa-angle-up');
     
-    $('.faq-links').find('i').removeClass('fas fa-arrow-up');
+    $('.faq-links').find('i').removeClass('fas fa-angle-up');
     
-    $('.faq-links').find('i').addClass('fas fa-arrow-down');
+    $('.faq-links').find('i').addClass('fas fa-angle-down');
     if(collapsed)
-    $(this).find('i').toggleClass('fas fa-arrow-up fas fa-arrow-down');
+    $(this).find('i').toggleClass('fas fa-angle-up fas fa-angle-down');
     visible = false;
     }
 });
