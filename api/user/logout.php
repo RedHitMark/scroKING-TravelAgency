fe4f675cfb638c3272abff50d761f695698f103b
ChristianDiscenza <christian.discenza@gmail.com>
2019-08-14 16:54:38 +0200

    aggiunto json a logout

--- api/user/logout.php
+++ api/user/logout.php
@@ -1,7 +1,9 @@
 <?php
     session_start();
     session_destroy();
+
+    echo json_encode(array("message" => "logout effettuata correttamente."));
  
     header("location:/index.htm");
     exit();
-?>
+    ?>
