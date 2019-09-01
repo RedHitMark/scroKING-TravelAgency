0c4b0b5a6401561994d718f8c5d5fb300abb4879
RedHitMark <russodivito.marco@gmail.com>
2019-09-01 20:36:20 +0200

    Piccolezza modificata

--- api/user/login.php
+++ api/user/login.php
@@ -8,11 +8,11 @@
 
     //include
     include_once("../config/MongoDB.php");
-    include_once("../models/LoginLog.php");
     include_once("../config/timestamp.php");
     include_once("../config/Session.php");
     include_once("../config/client.php");
     include_once("../config/security.php");
+    include_once("../models/LoginLog.php");
 
     //login params from http body
     $params = json_decode(file_get_contents("php://input"));
