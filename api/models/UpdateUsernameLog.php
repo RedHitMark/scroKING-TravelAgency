12e919c74db0d3e4322aae59a7d3c3f9c56234db
Stefano Fagnano <stefano@iMac-di-Stefano.station>
2019-09-15 18:59:51 +0200

    creato log username

---
+++ api/models/UpdateUsernameLog.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once("Log.php");
+
+class UpdateUsernameLog extends Log{
+
+    public $userId;
+    public $result;
+
+    /**
+     * LoginLog constructor.
+     * @param $timestamp
+     * @param $ip
+     * @param $userId
+     * @param $result
+     */
+    public function __construct($timestamp, $ip, $userId, $result) {
+        parent::__construct($timestamp, $ip);
+        $this->userId = $userId;
+        $this->result = $result;
+    }
+
+
+}
+
+
+?>
