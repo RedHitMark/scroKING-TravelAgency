f2bf1993b5e63dab68a7d3e95c7b43fe507989e2
stefanofagnano <Frutta12>
2019-09-19 17:58:53 +0200

    roba per marco

--- api/travel/insert_travel.php
+++ api/travel/insert_travel.php
@@ -12,9 +12,9 @@
     include_once("../models/Travel.php");
     include_once("../models/Veicle.php");
     include_once("../models/Destination.php");
-    include_once ("../config/Mail.php");
-    include_once ("../config/timestamp.php");
-    include_once ("../config/security.php");
+    include_once("../config/Mail.php");
+    include_once("../config/timestamp.php");
+    include_once("../config/security.php");
     
 
     $new_travel = json_decode(file_get_contents("php://input"));
@@ -22,21 +22,29 @@
     try{
         $mongo = new MongoDB();
 
-        if(isset($new_travel->type) && isset($new_travel->destination)&& isset($new_travel->startdata) && isset($new_travel->finishdata) && isset($new_travel->price) && isset($new_travel->viecles) && isset($new_travel->hotels)){
+
+        if(isset($new_travel->type) && isset($new_travel->destination) && isset($new_travel->startdata) && isset($new_travel->finishdata) && isset($new_travel->price)){
+
+            print_r($new_travel);
+
             $destination = new Destination(
                 $new_travel->destination->city1, $new_travel->destination->city2
             );
-            $doc = new Travel(
-                $mongo->getNewIdObject(),
-                $new_travel->tyoe, $destination, $new_travel->startdata,
-                $new_travel->finshdata, $new_travel->price,
-                $new_travel->veicles, $new_travel->hotels);
-            
-                $mongo->WriteOneQuery("scroKING", "Viaggi", $doc);   
 
-                 //response: 200  Success
-                 http_response_code(200);
-                 echo json_encode(array("message" => "Viggio inserito con successo"));
+            //$veicle = new Veicle("id", "test_name", "test_description", "test_seats", "test_price");
+            //$veicle = new Veicle($new_travel->name, $new_travel->description, $new_travel->seats, $new_travel->vprice);
+
+            //$hotel = new Hotel();
+            $doc = new Travel(
+            $mongo->getNewIdObject(),
+            $new_travel->type, $destination, $new_travel->startdata,
+            $new_travel->finishdata, $new_travel->price, ["1", "2"], ["1", "2"]);
+        
+            $mongo->WriteOneQuery("scroKING", "Viaggi", $doc);   
+
+            //response: 200  Success
+            http_response_code(200);
+            echo json_encode(array("message" => "Viggio inserito con successo"));
 
         } else {
             //response: 400 Bad Request
