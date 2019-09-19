811bed03b3c5d92799f109d527c44fa2fe46e5c9
RedHitMark <russodivito.marco@gmail.com>
2019-09-19 22:24:34 +0200

    Query read one

---
+++ api/hotel/read_one_hotel.php
@@ -0,0 +1,35 @@
+<?php
+    //headers
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    //include
+    include_once("../config/MongoDB.php");
+    include_once("../models/Hotel.php");
+
+    $params = json_decode(file_get_contents("php://input"));
+
+    try{
+        if ( isset($params->id) ) {
+            //new mongo instance
+            $mongo = new MongoDB();
+
+            //read all hotels from db
+            $result = $mongo->ReadOneQuery("scroKING", "Hotels", $params->id);
+
+            //response: 200  Success
+            http_response_code(200);
+            echo json_encode($result);
+        } else {
+            //response: 400 Bad Request
+            http_response_code(400);
+            echo json_encode(array("message" => "Parametri mancanti."));
+        }
+    }catch (Exception | MongoDB\Driver\Exception\Exception $e) {
+        //response: 500 Internal Server Error
+        http_response_code(500);
+        echo json_encode(array("message" => "Errore lato server.", "verbose" => $e->getMessage()));
+    }
