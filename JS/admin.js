62f081920fed892014bf09f4855a0a62204e07fe
stefanofagnano <Frutta12>
2019-09-20 11:01:15 +0200

    fixing

--- JS/admin.js
+++ JS/admin.js
@@ -26,7 +26,7 @@ $("#invia-riepilogo").click(function(){
     var tipologiaMezzo = $("#tipologia-mezzo").val();
     var nomeMezzo = $("#nome-mezzo").val();
     var descrizioneMezzo = $("#descrizione-mezzo").val();
-    var trattaMezzo = $("#tratta-mezzo").val();
+    
     /* Hotels */
     var nomeHotel = $('#nome-hotel').val();
     var descrizioneHotel = $('#descrizione-hotel').val();
@@ -36,7 +36,10 @@ $("#invia-riepilogo").click(function(){
     var camerelibereHotel = $('#camerelibere-hotel').val();
     /* Report message and insert call */
     if (type == 'mezzi') {
-        $( "#report" ).html("<h1 style='text-transform: uppercase; text-align: center; font-weight: bold;'>Riepilogo informazioni inserite:</h1>" + "<br>" + "<b>Tipologia nuovo elemento: </b> " + type + "<br>" + "<b>Posti del mezzo: </b>" + postiMezzo + "<br>" + "<b>Costo del mezzo:</b> " + costoMezzo + "<br>" + "<b>Tipologia del mezzo: </b>" + tipologiaMezzo + "<br>" + "<b>Nome del mezzo</b>" + nomeMezzo + "<br>" + "<b>Descrizione del mezzo</b>" + descrizioneMezzo + "<br>" + "<b>Tratta del mezzo</b>" + trattaMezzo );
+        $( "#report" ).html("<h1 style='text-transform: uppercase; text-align: center; font-weight: bold;'>Riepilogo informazioni inserite:</h1>" + "<br>" + "<b>Tipologia nuovo elemento: </b> " + type + "<br>" + "<b>Posti del mezzo: </b>" + postiMezzo + "<br>" + "<b>Costo del mezzo:</b> " + costoMezzo + "<br>" + "<b>Tipologia del mezzo: </b>" + tipologiaMezzo + "<br>" + "<b>Nome del mezzo</b>" + nomeMezzo + "<br>" + "<b>Descrizione del mezzo</b>" + descrizioneMezzo );
+
+        insert_veicle();
+
     } else if( type == 'hotel') {
         $('#report').html("<h1 style='text-transform: uppercase; text-align: center; font-weight: bold;'>Riepilogo informazioni inserite:</h1>" + "<br>" + "<b>Tipologia nuovo elemento: </b> " + type + "<br>" + "<b>Nome hotel: </b>" + nomeHotel + "<br>" + "<b>Descrizione hotel: </b>" + descrizioneHotel + "<br>" + "<b>Indirizzo Hotel: </b>" + indirizzoHotel + "<br>" + "<b>Telefono hotel: </b>" + telefonoHotel + "<br>" + "<b>E-mail hotel: </b>" + emailHotel + "<br>" + "<b>Camere libere: </b>" + camerelibereHotel);
         insert_hotel()
@@ -82,4 +85,35 @@ function insert_hotel() {
     console.log(hotel_data);
 
     post("api/hotel/insert_hotel.php", hotel_data, functions);
+}
+
+function insert_veicle() {
+
+    function insert_ok() {
+        alert("Inserimento avvenuto con successo");
+    }
+
+    function insert_bad_request() {
+        alert("Compila tutti i campi")
+    }
+
+    function insert_internal_server_error() {
+        alert("Riprova più tardi");
+    }
+
+    let functions = {
+        200: insert_ok,
+        400: insert_bad_request,
+        500: insert_internal_server_error
+    };
+
+    let veicle_data = {
+        name: $("#nome-mezzo").val(),
+        description : $("#descrizione-mezzo").val(),
+        type : $("#tipologia-mezzo").val(),
+        seats : $("#posti-mezzo").val(),
+        price : $("#costo-mezzo").val()
+    }
+    
+    post("api/veicle/insert_veicle.php",veicle_data, functions)
 }
