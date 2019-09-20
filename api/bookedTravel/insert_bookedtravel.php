6b51df8b57c417c1c195f6224ff1a5afdd909750
stefanofagnano <Frutta12>
2019-09-20 12:44:05 +0200

    fixing

--- api/bookedTravel/insert_bookedtravel.php
+++ api/bookedTravel/insert_bookedtravel.php
@@ -14,6 +14,7 @@
   include_once("../models/BookedTravel.php");
   include_once("../models/Destination.php");
   include_once("../config/Mail.php");
+  include_once("/Users/stefanofagnano/Sites/scroKING-TravelAgency/api/config/Session.php");
   include_once("../config/timestamp.php");
   include_once("../config/security.php");
   
@@ -22,19 +23,25 @@
 
   try{
 
-    if(isset($params->id_travel)){
+    if(isset($params->id_travel) && strlen ( $params->id_travel ) >= 24){
         
+      
         $session = new Session();
             if ($session->isSet("id")){
-
+                
+                
+                
                 $mongo = new MongoDB();
                 
-                $result_with_existent_travel_id = $mongo->$mongo->ReadQuery("scroKING", "Travels", ["_id" => $params->id_travel]);
+                $result_with_existent_travel_id =$mongo->ReadOneQuery("scroKING", "Travels", $params->id_travel);
 
 
-                if($result_with_existent_travel_id == 1){
+                if($result_with_existent_travel_id){
+                    $id_user = $session->get("id");
+                    $doc = new BlookedTravel( $mongo->getNewIdObject() ,$params->id_travel, $id_user);
 
-                    
+                    $mongo->WriteOneQuery("scroKING", "BookedTravels", $doc);   
+
 
                 }else{
                      //response: 406 Not Acceptable
@@ -51,7 +58,7 @@
     }else{
                     //response: 400 Bad Request
                     http_response_code(400);
-                    echo json_encode(array("message" => "Parametri mancanti."));
+                    echo json_encode(array("message" => "Parametri mancanti o errati."));
 
     }
 
