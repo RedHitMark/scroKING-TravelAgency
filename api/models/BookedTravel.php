915c62aabb613ac2d0fff5b89eb8d288c968da11
stefanofagnano <Frutta12>
2019-09-20 11:36:20 +0200

    aggiunti viaggi prenotati

---
+++ api/models/BookedTravel.php
@@ -0,0 +1,28 @@
+<?php
+
+class BlookedTravdl{
+    public $_id;
+    public $id_user;
+    public $id_travel;
+    public $id_transaction;
+
+    public function __construct($_id,$id_user,$id_travel,$id_transaction){
+        $this->_id = $_id;
+        $this->id_user = $id_user;
+        $this->id_travel = $id_travel;
+        $this->id_transaction = $id_transaction;
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+?>
