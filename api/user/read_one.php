07d4c8d4858371a20220127151f01a02588613af
RedHitMark <russodivito.marco@gmail.com>
2019-09-20 12:11:08 +0200

    Fix variabili

--- api/user/read_one.php
+++ api/user/read_one.php
@@ -17,14 +17,17 @@
 
         if(isset($_SESSION['id']) && isset($_SESSION['timestamp'])){
 
-            $query = new MongoDB();
+            $mongo = new MongoDB();
 
-            $result = $query->ReadOneQuery("scroKING", "Users", $session->get("id"), ["name","surname","username","email","address","role"]);
+            $user_result = $mongo->ReadOneQuery("scroKING", "Users", $session->get("id"), ["name","surname","username","email","address","role"]);
+
+            $booked_travels_result = $mongo->ReadQuery("scroKING", "BookedTravels", ["id_user" => $session->get("id")]);
+
+            //var_dump($booked_travels_result);
 
             // response: 200 OK
             http_response_code(200);
-            echo json_encode($result);
-
+            echo json_encode($user_result);
         }else{
             // response: 401 Unauthorized
             http_response_code(401);
